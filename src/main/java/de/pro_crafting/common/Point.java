f62cfd3ca52760a638e4fdabaaa7de92f22aa904
Postremus <postremus1996@googlemail.com>
2014-08-09 16:11:53 +0200

    added autogenerated hashCode and equals methods

--- src/main/java/de/pro_crafting/common/Point.java
+++ src/main/java/de/pro_crafting/common/Point.java
@@ -63,7 +63,7 @@ public class Point {
 	public void add(int x, int y, int z) {
 		this.x += x;
 		this.y += y;
-		this.z += z;
+		this.z += z;  
 	}
 	
 	public void add(Point value) {
@@ -90,5 +90,33 @@ public class Point {
 		this.x *= value;
 		this.y *= value;
 		this.z *= value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + this.x;
+		result = prime * result + this.y;
+		result = prime * result + this.z;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (this.x != other.x)
+			return false;
+		if (this.y != other.y)
+			return false;
+		if (this.z != other.z)
+			return false;
+		return true;
 	}
 }
