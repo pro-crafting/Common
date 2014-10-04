8d3baba75a0091e625fb583dd87475773d32e915
Postremus <postremus1996@googlemail.com>
2014-10-04 12:32:58 +0200

    Added Size Class

---
+++ src/main/java/de/pro_crafting/common/Size.java
@@ -0,0 +1,65 @@
+package de.pro_crafting.common;
+
+public class Size {
+	private int height;
+	private int width;
+	private int depth;
+	
+	public Size(int height, int width, int depth) {
+		this.height = height;
+		this.width = width;
+		this.depth = depth;
+	}	
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getDepth() {
+		return this.depth;
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + this.depth;
+		result = prime * result + this.height;
+		result = prime * result + this.width;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Size other = (Size) obj;
+		if (this.depth != other.depth)
+			return false;
+		if (this.height != other.height)
+			return false;
+		if (this.width != other.width)
+			return false;
+		return true;
+	}
+}
